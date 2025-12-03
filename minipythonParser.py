301b31433c6d2f50cec339e9362babc63297b580
Zayan <zayan.qazi.mail@gmail.com>
2025-12-03 11:50:35 -0600

    I think test_parser.py is working. Except in line 33, parser.getNumberOfSyntaxErrors() is not working. The method doesn't exist

--- minipythonParser.py
+++ minipythonParser.py
@@ -5,23 +5,38 @@ from io import StringIO
 
 def serializedATN():
     with StringIO() as buf:
-        buf.write("\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\f")
-        buf.write("\61\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\3\2\3\2\3\2\3\3\3")
-        buf.write("\3\3\3\7\3\21\n\3\f\3\16\3\24\13\3\3\4\3\4\3\4\3\4\3\4")
-        buf.write("\3\4\3\4\3\4\3\4\5\4\37\n\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
-        buf.write("\4\3\4\3\4\7\4*\n\4\f\4\16\4-\13\4\3\5\3\5\3\5\2\3\6\6")
-        buf.write("\2\4\6\b\2\3\4\2\6\6\13\13\63\2\n\3\2\2\2\4\22\3\2\2\2")
-        buf.write("\6\36\3\2\2\2\b.\3\2\2\2\n\13\5\4\3\2\13\f\7\2\2\3\f\3")
-        buf.write("\3\2\2\2\r\16\5\6\4\2\16\17\7\n\2\2\17\21\3\2\2\2\20\r")
-        buf.write("\3\2\2\2\21\24\3\2\2\2\22\20\3\2\2\2\22\23\3\2\2\2\23")
-        buf.write("\5\3\2\2\2\24\22\3\2\2\2\25\37\b\4\1\2\26\27\7\6\2\2\27")
-        buf.write("\30\7\3\2\2\30\37\5\6\4\b\31\37\5\b\5\2\32\33\7\4\2\2")
-        buf.write("\33\34\5\6\4\2\34\35\7\5\2\2\35\37\3\2\2\2\36\25\3\2\2")
-        buf.write("\2\36\26\3\2\2\2\36\31\3\2\2\2\36\32\3\2\2\2\37+\3\2\2")
-        buf.write("\2 !\f\7\2\2!\"\7\7\2\2\"*\5\6\4\b#$\f\6\2\2$%\7\b\2\2")
-        buf.write("%*\5\6\4\7&\'\f\5\2\2\'(\7\t\2\2(*\5\6\4\6) \3\2\2\2)")
-        buf.write("#\3\2\2\2)&\3\2\2\2*-\3\2\2\2+)\3\2\2\2+,\3\2\2\2,\7\3")
-        buf.write("\2\2\2-+\3\2\2\2./\t\2\2\2/\t\3\2\2\2\6\22\36)+")
+        buf.write("\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\27")
+        buf.write("`\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\3\2\3\2\3\2\3\3\3\3")
+        buf.write("\3\3\7\3\21\n\3\f\3\16\3\24\13\3\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\7\4$\n\4\f\4\16\4\'")
+        buf.write("\13\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\7\4")
+        buf.write("\64\n\4\f\4\16\4\67\13\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\7\4D\n\4\f\4\16\4G\13\4\5\4I\n\4\3\4\3")
+        buf.write("\4\3\4\5\4N\n\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\7")
+        buf.write("\4Y\n\4\f\4\16\4\\\13\4\3\5\3\5\3\5\2\3\6\6\2\4\6\b\2")
+        buf.write("\3\4\2\f\r\20\21j\2\n\3\2\2\2\4\22\3\2\2\2\6M\3\2\2\2")
+        buf.write("\b]\3\2\2\2\n\13\5\4\3\2\13\f\7\2\2\3\f\3\3\2\2\2\r\16")
+        buf.write("\5\6\4\2\16\17\7\26\2\2\17\21\3\2\2\2\20\r\3\2\2\2\21")
+        buf.write("\24\3\2\2\2\22\20\3\2\2\2\22\23\3\2\2\2\23\5\3\2\2\2\24")
+        buf.write("\22\3\2\2\2\25N\b\4\1\2\26\27\7\f\2\2\27\30\7\3\2\2\30")
+        buf.write("N\5\6\4\f\31\32\7\4\2\2\32\33\5\6\4\2\33\34\7\5\2\2\34")
+        buf.write("N\3\2\2\2\35\36\7\4\2\2\36\37\5\6\4\2\37 \7\6\2\2 %\5")
+        buf.write("\6\4\2!\"\7\6\2\2\"$\5\6\4\2#!\3\2\2\2$\'\3\2\2\2%#\3")
+        buf.write("\2\2\2%&\3\2\2\2&(\3\2\2\2\'%\3\2\2\2()\7\5\2\2)N\3\2")
+        buf.write("\2\2*+\7\4\2\2+,\5\6\4\2,-\7\6\2\2-.\7\5\2\2.N\3\2\2\2")
+        buf.write("/\60\7\7\2\2\60\65\5\6\4\2\61\62\7\6\2\2\62\64\5\6\4\2")
+        buf.write("\63\61\3\2\2\2\64\67\3\2\2\2\65\63\3\2\2\2\65\66\3\2\2")
+        buf.write("\2\668\3\2\2\2\67\65\3\2\2\289\7\b\2\29N\3\2\2\2:;\7\t")
+        buf.write("\2\2;<\5\6\4\2<H\7\n\2\2=E\5\6\4\2>?\7\6\2\2?@\5\6\4\2")
+        buf.write("@A\7\n\2\2AB\5\6\4\2BD\3\2\2\2C>\3\2\2\2DG\3\2\2\2EC\3")
+        buf.write("\2\2\2EF\3\2\2\2FI\3\2\2\2GE\3\2\2\2H=\3\2\2\2HI\3\2\2")
+        buf.write("\2IJ\3\2\2\2JK\7\13\2\2KN\3\2\2\2LN\5\b\5\2M\25\3\2\2")
+        buf.write("\2M\26\3\2\2\2M\31\3\2\2\2M\35\3\2\2\2M*\3\2\2\2M/\3\2")
+        buf.write("\2\2M:\3\2\2\2ML\3\2\2\2NZ\3\2\2\2OP\f\13\2\2PQ\7\22\2")
+        buf.write("\2QY\5\6\4\fRS\f\n\2\2ST\7\23\2\2TY\5\6\4\13UV\f\t\2\2")
+        buf.write("VW\7\24\2\2WY\5\6\4\nXO\3\2\2\2XR\3\2\2\2XU\3\2\2\2Y\\")
+        buf.write("\3\2\2\2ZX\3\2\2\2Z[\3\2\2\2[\7\3\2\2\2\\Z\3\2\2\2]^\t")
+        buf.write("\2\2\2^\t\3\2\2\2\n\22%\65EHMXZ")
         return buf.getvalue()
 
 
@@ -35,10 +50,14 @@ class minipythonParser ( Parser ):
 
     sharedContextCache = PredictionContextCache()
 
-    literalNames = [ "<INVALID>", "'='", "'('", "')'" ]
+    literalNames = [ "<INVALID>", "'='", "'('", "')'", "','", "'['", "']'", 
+                     "'{'", "':'", "'}'" ]
 
     symbolicNames = [ "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
-                      "ID", "OP_1", "OP_2", "OP_3", "NEWLINE", "INT", "WS" ]
+                      "<INVALID>", "<INVALID>", "<INVALID>", "<INVALID>", 
+                      "<INVALID>", "<INVALID>", "ID", "NUMBER", "FLOAT", 
+                      "INT", "BOOL", "STRING", "OP_1", "OP_2", "OP_3", "COMMENT", 
+                      "NEWLINE", "WS" ]
 
     RULE_prog = 0
     RULE_block = 1
@@ -51,13 +70,24 @@ class minipythonParser ( Parser ):
     T__0=1
     T__1=2
     T__2=3
-    ID=4
-    OP_1=5
-    OP_2=6
-    OP_3=7
-    NEWLINE=8
-    INT=9
-    WS=10
+    T__3=4
+    T__4=5
+    T__5=6
+    T__6=7
+    T__7=8
+    T__8=9
+    ID=10
+    NUMBER=11
+    FLOAT=12
+    INT=13
+    BOOL=14
+    STRING=15
+    OP_1=16
+    OP_2=17
+    OP_3=18
+    COMMENT=19
+    NEWLINE=20
+    WS=21
 
     def __init__(self, input:TokenStream):
         super().__init__(input)
@@ -221,11 +251,12 @@ class minipythonParser ( Parser ):
         _prevctx = localctx
         _startState = 4
         self.enterRecursionRule(localctx, 4, self.RULE_expr, _p)
+        self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 28
+            self.state = 75
             self._errHandler.sync(self)
-            la_ = self._interp.adaptivePredict(self._input,1,self._ctx)
+            la_ = self._interp.adaptivePredict(self._input,5,self._ctx)
             if la_ == 1:
                 pass
 
@@ -235,79 +266,171 @@ class minipythonParser ( Parser ):
                 self.state = 21
                 self.match(minipythonParser.T__0)
                 self.state = 22
-                self.expr(6)
+                self.expr(10)
                 pass
 
             elif la_ == 3:
                 self.state = 23
-                self.atom()
+                self.match(minipythonParser.T__1)
+                self.state = 24
+                self.expr(0)
+                self.state = 25
+                self.match(minipythonParser.T__2)
                 pass
 
             elif la_ == 4:
-                self.state = 24
+                self.state = 27
                 self.match(minipythonParser.T__1)
-                self.state = 25
+                self.state = 28
                 self.expr(0)
-                self.state = 26
+                self.state = 29
+                self.match(minipythonParser.T__3)
+                self.state = 30
+                self.expr(0)
+                self.state = 35
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+                while _la==minipythonParser.T__3:
+                    self.state = 31
+                    self.match(minipythonParser.T__3)
+                    self.state = 32
+                    self.expr(0)
+                    self.state = 37
+                    self._errHandler.sync(self)
+                    _la = self._input.LA(1)
+
+                self.state = 38
                 self.match(minipythonParser.T__2)
+                pass
+
+            elif la_ == 5:
+                self.state = 40
+                self.match(minipythonParser.T__1)
+                self.state = 41
+                self.expr(0)
+                self.state = 42
+                self.match(minipythonParser.T__3)
+                self.state = 43
+                self.match(minipythonParser.T__2)
+                pass
+
+            elif la_ == 6:
+                self.state = 45
+                self.match(minipythonParser.T__4)
+                self.state = 46
+                self.expr(0)
+                self.state = 51
+                self._errHandler.sync(self)
+                _la = self._input.LA(1)
+                while _la==minipythonParser.T__3:
+                    self.state = 47
+                    self.match(minipythonParser.T__3)
+                    self.state = 48
+                    self.expr(0)
+                    self.state = 53
+                    self._errHandler.sync(self)
+                    _la = self._input.LA(1)
+
+                self.state = 54
+                self.match(minipythonParser.T__5)
+                pass
+
+            elif la_ == 7:
+                self.state = 56
+                self.match(minipythonParser.T__6)
+                self.state = 57
+                self.expr(0)
+                self.state = 58
+                self.match(minipythonParser.T__7)
+                self.state = 70
+                self._errHandler.sync(self)
+                la_ = self._interp.adaptivePredict(self._input,4,self._ctx)
+                if la_ == 1:
+                    self.state = 59
+                    self.expr(0)
+                    self.state = 67
+                    self._errHandler.sync(self)
+                    _la = self._input.LA(1)
+                    while _la==minipythonParser.T__3:
+                        self.state = 60
+                        self.match(minipythonParser.T__3)
+                        self.state = 61
+                        self.expr(0)
+                        self.state = 62
+                        self.match(minipythonParser.T__7)
+                        self.state = 63
+                        self.expr(0)
+                        self.state = 69
+                        self._errHandler.sync(self)
+                        _la = self._input.LA(1)
+
+
+
+                self.state = 72
+                self.match(minipythonParser.T__8)
+                pass
+
+            elif la_ == 8:
+                self.state = 74
+                self.atom()
                 pass
 
 
             self._ctx.stop = self._input.LT(-1)
-            self.state = 41
+            self.state = 88
             self._errHandler.sync(self)
-            _alt = self._interp.adaptivePredict(self._input,3,self._ctx)
+            _alt = self._interp.adaptivePredict(self._input,7,self._ctx)
             while _alt!=2 and _alt!=ATN.INVALID_ALT_NUMBER:
                 if _alt==1:
                     if self._parseListeners is not None:
                         self.triggerExitRuleEvent()
                     _prevctx = localctx
-                    self.state = 39
+                    self.state = 86
                     self._errHandler.sync(self)
-                    la_ = self._interp.adaptivePredict(self._input,2,self._ctx)
+                    la_ = self._interp.adaptivePredict(self._input,6,self._ctx)
                     if la_ == 1:
                         localctx = minipythonParser.ExprContext(self, _parentctx, _parentState)
                         self.pushNewRecursionContext(localctx, _startState, self.RULE_expr)
-                        self.state = 30
-                        if not self.precpred(self._ctx, 5):
+                        self.state = 77
+                        if not self.precpred(self._ctx, 9):
                             from antlr4.error.Errors import FailedPredicateException
-                            raise FailedPredicateException(self, "self.precpred(self._ctx, 5)")
-                        self.state = 31
+                            raise FailedPredicateException(self, "self.precpred(self._ctx, 9)")
+                        self.state = 78
                         self.match(minipythonParser.OP_1)
-                        self.state = 32
-                        self.expr(6)
+                        self.state = 79
+                        self.expr(10)
                         pass
 
                     elif la_ == 2:
                         localctx = minipythonParser.ExprContext(self, _parentctx, _parentState)
                         self.pushNewRecursionContext(localctx, _startState, self.RULE_expr)
-                        self.state = 33
-                        if not self.precpred(self._ctx, 4):
+                        self.state = 80
+                        if not self.precpred(self._ctx, 8):
                             from antlr4.error.Errors import FailedPredicateException
-                            raise FailedPredicateException(self, "self.precpred(self._ctx, 4)")
-                        self.state = 34
+                            raise FailedPredicateException(self, "self.precpred(self._ctx, 8)")
+                        self.state = 81
                         self.match(minipythonParser.OP_2)
-                        self.state = 35
-                        self.expr(5)
+                        self.state = 82
+                        self.expr(9)
                         pass
 
                     elif la_ == 3:
                         localctx = minipythonParser.ExprContext(self, _parentctx, _parentState)
                         self.pushNewRecursionContext(localctx, _startState, self.RULE_expr)
-                        self.state = 36
-                        if not self.precpred(self._ctx, 3):
+                        self.state = 83
+                        if not self.precpred(self._ctx, 7):
                             from antlr4.error.Errors import FailedPredicateException
-                            raise FailedPredicateException(self, "self.precpred(self._ctx, 3)")
-                        self.state = 37
+                            raise FailedPredicateException(self, "self.precpred(self._ctx, 7)")
+                        self.state = 84
                         self.match(minipythonParser.OP_3)
-                        self.state = 38
-                        self.expr(4)
+                        self.state = 85
+                        self.expr(8)
                         pass
 
              
-                self.state = 43
+                self.state = 90
                 self._errHandler.sync(self)
-                _alt = self._interp.adaptivePredict(self._input,3,self._ctx)
+                _alt = self._interp.adaptivePredict(self._input,7,self._ctx)
 
         except RecognitionException as re:
             localctx.exception = re
@@ -323,11 +446,17 @@ class minipythonParser ( Parser ):
             super().__init__(parent, invokingState)
             self.parser = parser
 
-        def INT(self):
-            return self.getToken(minipythonParser.INT, 0)
+        def NUMBER(self):
+            return self.getToken(minipythonParser.NUMBER, 0)
 
         def ID(self):
             return self.getToken(minipythonParser.ID, 0)
+
+        def STRING(self):
+            return self.getToken(minipythonParser.STRING, 0)
+
+        def BOOL(self):
+            return self.getToken(minipythonParser.BOOL, 0)
 
         def getRuleIndex(self):
             return minipythonParser.RULE_atom
@@ -350,9 +479,9 @@ class minipythonParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 44
+            self.state = 91
             _la = self._input.LA(1)
-            if not(_la==minipythonParser.ID or _la==minipythonParser.INT):
+            if not((((_la) & ~0x3f) == 0 and ((1 << _la) & ((1 << minipythonParser.ID) | (1 << minipythonParser.NUMBER) | (1 << minipythonParser.BOOL) | (1 << minipythonParser.STRING))) != 0)):
                 self._errHandler.recoverInline(self)
             else:
                 self._errHandler.reportMatch(self)
@@ -379,15 +508,15 @@ class minipythonParser ( Parser ):
 
     def expr_sempred(self, localctx:ExprContext, predIndex:int):
             if predIndex == 0:
-                return self.precpred(self._ctx, 5)
+                return self.precpred(self._ctx, 9)
          
 
             if predIndex == 1:
-                return self.precpred(self._ctx, 4)
+                return self.precpred(self._ctx, 8)
          
 
             if predIndex == 2:
-                return self.precpred(self._ctx, 3)
+                return self.precpred(self._ctx, 7)
          
 
 
